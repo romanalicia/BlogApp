63af328b94507a09153c32134853fca573f4a714
alicias-mac <AliciaRoman@Alicias-MacBook.local>
2024-05-17 14:22:33 +1200

    more changes

--- website/models.py
+++ website/models.py
@@ -15,5 +15,6 @@ class User(db.Model, UserMixin):
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
+    password = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
