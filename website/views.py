e095df2beaedc9a4b006f2dcd39c39c7bedaaa7b
alicias-mac <AliciaRoman@Alicias-MacBook.local>
2024-06-15 09:59:29 +1200

    making a post

--- website/views.py
+++ website/views.py
@@ -11,31 +11,3 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 def home():
     return render_template("home.html", user=current_user)
-
-
-@views.route("/posts", methods=['GET', 'POST'])
-def posts():
-    if request.method == 'POST':
-        posts = request.form.get('posts')  # Gets the post from HTML
-        if len(posts) < 1:
-            flash("Post is too short!", category='error')
-        else:
-            # providing the schema for the post
-            new_posts = Posts(data=posts, user_id=current_user.id)
-            db.session.add(new_posts)  # adding the post to the database
-            db.session.commit()
-            flash('Post added!', category='success')
-
-    return render_template("posts.html", user=current_user)
-
-
-@views.route('/delete-posts', methods=['POST'])
-def delete_posts():
-    posts = json.loads(request.data)
-    postsId = posts['postsId']
-    posts = Posts.query.get(postsId)
-    if posts:
-        if posts.user_id == current_user.id:
-            db.session.delete(posts)
-            db.session.commit()
-    return jsonify({})
