de96014576c949afde2160df3871b1169b9109a2
alicias-mac <AliciaRoman@Alicias-MacBook.local>
2024-06-15 10:46:06 +1200

    finished making a blog post (basics)

--- website/models.py
+++ website/models.py
@@ -15,8 +15,6 @@ class User(db.Model, UserMixin):
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
-    data = db.Column(db.String(10000))
-    password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,13 +1,30 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Posts
+from .models import Posts, User
 from .import db
-import json
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/", methods=['GET', 'POST'])
 @views.route("/home")
+@login_required
 def home():
     return render_template("home.html", user=current_user)
+
+
+@views.route("/posts", methods=['GET', 'POST'])
+@login_required
+def posts():
+    if request.method == "POST":
+        text = request.form.get('text')
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            posts = Posts(text=text, author=current_user.id)
+            db.session.add(posts)
+            db.session.commit()
+            flash('Post created!', category='success')
+        return redirect(url_for('views.posts'))
+
+    return render_template("posts.html", user=current_user)
