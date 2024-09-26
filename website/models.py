8560d96b7ba9a9be8cc262e4b7adba14c1e1c84d
alicias-mac <AliciaRoman@Alicias-MacBook.local>
2024-09-27 10:08:55 +1200

    folders

--- website/models.py
+++ website/models.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Posts', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
 
 
 class Posts(db.Model):
@@ -18,3 +20,24 @@ class Posts(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
+    likes = db.relationship('Like', backref='posts', passive_deletes=True)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    posts_id = db.Column(db.Integer, db.ForeignKey(
+        'posts.id', ondelete="CASCADE"), nullable=False)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    posts_id = db.Column(db.Integer, db.ForeignKey(
+        'posts.id', ondelete="CASCADE"), nullable=False)
